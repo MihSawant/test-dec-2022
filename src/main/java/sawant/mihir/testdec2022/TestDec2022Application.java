cd625f42bf7e79cf5c23e118bb27953c28040e0b
Mihir Sawant <mihirssawant06@gmail.com>
2023-06-05 12:06:09 +0530

    bootstrap classes added

--- src/main/java/sawant/mihir/testdec2022/TestDec2022Application.java
+++ src/main/java/sawant/mihir/testdec2022/TestDec2022Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TestDec2022Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TestDec2022Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TestDec2022Application.class, args);
+    }
 
 }
--- src/test/java/sawant/mihir/testdec2022/TestDec2022ApplicationTests.java
+++ src/test/java/sawant/mihir/testdec2022/TestDec2022ApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TestDec2022ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
