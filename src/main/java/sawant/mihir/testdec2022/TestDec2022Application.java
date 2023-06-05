93b36f563c7f32c91eda8ed9893a5467d515bbff
Mihir Sawant <mihirssawant06@gmail.com>
2023-06-05 12:03:16 +0530

    initial commit
    added all needed dependencies

---
+++ src/main/java/sawant/mihir/testdec2022/TestDec2022Application.java
@@ -0,0 +1,13 @@
+package sawant.mihir.testdec2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDec2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDec2022Application.class, args);
+	}
+
+}
